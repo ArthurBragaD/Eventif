153fb0f9c96d72588ea48107675c924fc5ee35cb
BragaD <arthurbragadutra@gmail.com>
2023-09-10 17:34:17 -0300

    teste do não req do telefone e textarea da msg

--- contact/forms.py
+++ contact/forms.py
@@ -4,5 +4,6 @@ class contatoForm(forms.Form):
     #Criação do form
     name = forms.CharField(label='Nome')
     email = forms.EmailField(label='Email')
-    phone = forms.CharField(label='Telefone')
-    message = forms.CharField(label='Mensagem')
+    phone = forms.CharField(label='Telefone', empty_value='Não informado')
+    message = forms.CharField(label='Mensagem',widget=forms.Textarea)
+    # phone.empty_label="Nothing"
--- contact/tests/test_mail_contact.py
+++ contact/tests/test_mail_contact.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 from django.core import mail
 
-class MailTestContato(TestCase):
+class MailTestContatoCompleto(TestCase):
+    #Definindo as variaveis 
     def setUp(self):
         data = dict(name='Arthur Braga', email='Arthurbragadutra@gmail.com', phone='53 91234-5678' , message='Testando contato')
         self.response = self.client.post('/contatoView/', data)
@@ -24,3 +25,29 @@ class MailTestContato(TestCase):
         for content in contents:
             with self.subTest():
                 self.assertIn(content, self.email.body)
+
+
+# class MailTestContatoSemTelefone(TestCase):
+#     #Definindo as variaveis sem telefone
+#     def setUp(self):
+#         data = dict(name='Arthur Braga', email='Arthurbragadutra@gmail.com', phone='' , message='Testando contato')
+#         self.response = self.client.post('/contatoView/', data)
+#         self.email = mail.outbox[0]
+
+#     def test_contact_email_subject(self):
+#         expect = 'Confirmação de envio de contato'
+#         self.assertEqual(expect, self.email.subject)
+
+#     def test_contact_email_sender(self):
+#         expect = 'contato@eventif.com.br'
+#         self.assertEqual(expect, self.email.from_email)
+
+#     def test_contact_email_to(self):
+#         expect = ['contato@eventif.com.br', 'Arthurbragadutra@gmail.com']
+#         self.assertEqual(expect, self.email.to)
+
+#     def test_contact_email_body(self):
+#         contents = ('Arthur Braga', 'Arthurbragadutra@gmail.com', '', 'Testando contato')
+#         for content in contents:
+#             with self.subTest():
+#                 self.assertIn(content, self.email.body)
