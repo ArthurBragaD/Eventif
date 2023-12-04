4000106f5a01365f37cd9d48296a17c86afea852
BragaD <arthurbragadutra@gmail.com>
2023-12-04 19:11:27 -0300

    Teste para resposta de email

--- contact/admin.py
+++ contact/admin.py
@@ -1,19 +1,40 @@
 from django.contrib import admin
+from django.conf import settings
 from django.utils.timezone import now
 from contact.models import Contact
-
+from django.core import mail
+from django.shortcuts import render
+from django.conf import settings
+from django.contrib import messages
+from django.core import mail
+from django.http import HttpResponseRedirect
+from django.template.loader import render_to_string
 
 class ContactModelAdmin(admin.ModelAdmin):
-    list_display = ['name', 'email', 'phone',
-                    'created_at', 'updated_at', 'message', 'response']
+    list_display = ['name', 'email', 'phone', 'created_at', 'updated_at', 'message', 'response']
     date_hierarchy = 'created_at'
     search_fields = ['name', 'email', 'phone', 'created_at', 'updated_at', 'message', 'response']
     list_filter = ['created_at', 'updated_at']
 
-    # def contacted_today(self, obj):
-    #     return obj.created_at.date() == now().date()
+    actions = ["responder"]
 
-    # contacted_today.short_description = 'contatado hoje?'
-    # contacted_today.boolean = True
+    def responder(self, request, queryset):
+        resposta = "Mensagem foi marcada como respondida"
+        for contact in queryset:
+            nome = contact.name
+            emailEnvia = settings.DEFAULT_FROM_EMAIL
+            emailRecebe = [contact.email , settings.DEFAULT_FROM_EMAIL]
+            corpoDoEmail = render_to_string('contact/contact_response.txt', {'contact': contact})
+            mail.send_mail(
+                nome,
+                corpoDoEmail,
+                emailEnvia,
+                emailRecebe,
+                resposta,
+            )
+            queryset.update(response=True)
+        self.message_user(request, f'{queryset.count()} contato(s) foi(ram) respondido(s) com sucesso.')
+
+    responder.short_description = 'Enviar resposta para contatos selecionados'
 
 admin.site.register(Contact, ContactModelAdmin)
