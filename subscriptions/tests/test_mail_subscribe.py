9548f8433ac9f07fae1db132c4df18b3251602de
BragaD <arthurbragadutra@gmail.com>
2023-12-04 15:23:37 -0300

    Botando em dia

--- subscriptions/tests/test_mail_subscribe.py
+++ subscriptions/tests/test_mail_subscribe.py
@@ -1,18 +1,24 @@
 from django.test import TestCase
 from django.core import mail
+from django.shortcuts import resolve_url as r
+
 
 class MailTest(TestCase):
     def setUp(self):
-        data = dict(name='Arthur Braga', cpf='12345678901', email='Arthurbragadutra@gmail.com', phone='53 91234-5678')
-        self.response = self.client.post('/inscricao/', data)
+        data = dict(name="Arthur Braga",
+                    cpf="12345678901",
+                    email="Arthurbragadutra@gmail.com",
+                    phone="53-91234-5678")
+
+        self.response = self.client.post(r('subscriptions:new'), data)
         self.email = mail.outbox[0]
 
     def test_subscription_email_subject(self):
-        expect = 'Confirmação de inscrição'
+        expect = "Confirmação de inscrição"
         self.assertEqual(expect, self.email.subject)
 
     def test_subscription_email_sender(self):
-        expect = 'contato@eventif.com.br'
+        expect = "contato@eventif.com.br"
         self.assertEqual(expect, self.email.from_email)
 
     def test_subscription_email_to(self):
@@ -20,7 +26,10 @@ class MailTest(TestCase):
         self.assertEqual(expect, self.email.to)
 
     def test_subscription_email_body(self):
-        contents = ('Arthur Braga', '12345678901', 'Arthurbragadutra@gmail.com', '53 91234-5678')
+        contents = ['Arthur Braga',
+                    '12345678901',
+                    'Arthurbragadutra@gmail.com',
+                    '53-91234-5678']
         for content in contents:
             with self.subTest():
                 self.assertIn(content, self.email.body)
