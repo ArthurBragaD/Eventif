dd5dd8a5748161a51f103ecfb49e21186e9116be
BragaD <arthurbragadutra@gmail.com>
2023-12-04 15:27:02 -0300

    Botando em dia

--- contact/tests/test_form_contact.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-from contact.forms import contatoForm
-
-class contatoFormTest(TestCase):
-    def setUp(self):
-        self.form = contatoForm()
-
-    def testFormInformacoes(self):
-        self.assertSequenceEqual(['name', 'email', 'phone', 'message'], list(self.form.fields))
